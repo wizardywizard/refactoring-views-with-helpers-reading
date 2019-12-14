ab5ca03d31c785783ff4ac3049f1d7e6811be2ad
wizardywizard <littledavid175@gmail.com>
2019-12-14 13:04:08 -0800

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,9 @@ class Post < ActiveRecord::Base
   belongs_to :author
 
   private
+  def last_updated
+    updated_at.strftime("Last updated %A, %b %e, at %l:%M %p")
+  end
 
   def is_title_case
     if title.split.any?{|w|w[0].upcase != w[0]}
